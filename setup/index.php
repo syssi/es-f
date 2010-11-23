5e5b18499a462cdbb1e658d0017ca6cfa10935ae
Knut Kohl <knutkohl@users.sourceforge.net>
2010-11-23 22:37:05 +0100

    missed some .htaccess files

--- setup/index.php
+++ setup/index.php
@@ -20,10 +20,10 @@ $steps = array(
 );
 // -----------------------------------------------------------------------------
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-#error_reporting(E_ALL);
+#ini_set('display_startup_errors', 1);
+#ini_set('display_errors', 1);
+#error_reporting(-1);
+error_reporting(0);
 
 session_start();
 
