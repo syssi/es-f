f9a2b138ab4a9a0f9a2fa07eccf9bf64a9089e76
Knut Kohl <knutkohl@users.sourceforge.net>
2011-09-30 21:57:54 +0200

    Revise auction refresh logic, move from plugin to index action

--- module/refresh/module.class.php
+++ module/refresh/module.class.php
@@ -20,15 +20,37 @@ class esf_Module_Refresh extends esf_Module {
    *
    */
   public function IndexAction() {
-    // set marker for plugin
-    if (!Session::get('Module.Refresh.Items')) {
+    esf_Auctions::Load();
+
+    if ($auctions = Session::takeout('Module.Refresh.Items')) {
+      // check auction existence
+      foreach ($auctions as $id=>$item) {
+        if (!isset(esf_Auctions::$Auctions[$item])) {
+          unset($auctions[$id]);
+        }
+      }
+    } else {
       // all auctions
-      esf_Auctions::Load();
-      Session::set('Module.Refresh.Items', array_keys(esf_Auctions::$Auctions));
-      Event::ProcessInform('setLastUpdate');
+      $auctions = array_keys(esf_Auctions::$Auctions);
     }
 
-    // redirect to last module
+    if (!empty($auctions)) {
+      // refresh...
+      $r = 0;
+      foreach ($auctions as $item) {
+        $auction = esf_Auctions::get($item);
+        if (!$auction['ended'] AND
+            $auction = esf_Auctions::fetchAuction($auction, $this->FullRefresh)) {
+          esf_Auctions::set($auction, FALSE);
+          esf_Auctions::Save($auction, FALSE);
+          $r++;
+        }
+      }
+      Event::ProcessInform('setLastUpdate');
+      Messages::Success(Translation::get('Refresh.Done', $r, count($auctions)-$r));
+    }
+
+    // redirect to last module, mostly "auction"
     $lastmodule = Session::get('Module.Refresh.Module');
     $this->redirect($lastmodule ? $lastmodule : STARTMODULE);
   }
--- module/refresh/plugin.class.php
+++ module/refresh/plugin.class.php
@@ -16,7 +16,7 @@ class esf_Plugin_Module_Refresh extends esf_Plugin {
    * @return array Array of events handled by the plugin
    */
   public function handles() {
-    return array('LanguageSet', 'BuildMenu', 'ProcessStart');
+    return array('LanguageSet', 'BuildMenu');
   }
 
   /**
@@ -32,47 +32,6 @@ class esf_Plugin_Module_Refresh extends esf_Plugin {
       Session::set('Module.Refresh.Module', Registry::get('esf.Module'));
   }
 
-  /**
-   *
-   */
-  function ProcessStart() {
-    if (!esf_User::isValid()) return;
-
-    // skip refresh, when only content is required (e.g. auction / group edit)
-    if (Registry::get('esf.contentonly')) return;
-
-    $maxage = (int) Registry::get('Module.Refresh.MaxAge') * 60;
-
-    if (Registry::get('esf.module') == 'auction' AND $maxage > 0 AND
-        $_SERVER['REQUEST_TIME']-$maxage > Event::ProcessReturn('getLastUpdate')) {
-      TplData::add('HtmlHeader.raw', StylesAndScripts('module/refresh', Session::get('Layout')));
-      $items = array_keys(esf_Auctions::$Auctions);
-    } else {
-      $items = Session::get('Module.Refresh.Items', NULL, TRUE);
-    }
-
-    if (empty($items)) return;
-
-    $auctions = array();
-    foreach ($items as $item) {
-      if (isset(esf_Auctions::$Auctions[$item])) $auctions[] = $item;
-    }
-
-    if (!$count = count($auctions)) return;
-
-    $r = 0;
-    foreach ($auctions as $item) {
-      $auction = esf_Auctions::get($item);
-      if (!$auction['ended'] AND
-          $auction = esf_Auctions::fetchAuction($auction, $this->FullRefresh)) {
-        esf_Auctions::set($auction, FALSE);
-        esf_Auctions::Save($auction, FALSE);
-        $r = $r + 1;
-      }
-    }
-    Event::ProcessInform('setLastUpdate');
-    Messages::Success(Translation::get('Refresh.Done', $r, $count-$r));
-  }
 }
 
 Event::attach(new esf_Plugin_Module_Refresh, 100);
