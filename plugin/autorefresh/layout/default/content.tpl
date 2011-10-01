3e150a86dbcbb8a930b8ae2019d7249d9208106d
Knut Kohl <knutkohl@users.sourceforge.net>
2011-10-01 21:25:56 +0200

    Refactoring of logic

--- plugin/autorefresh/layout/default/content.tpl
+++ plugin/autorefresh/layout/default/content.tpl
@@ -1,24 +1,24 @@
 <div class="noprint" style="text-align:center;padding:5px;font-size:90%">
   {form:}
-  <!-- IF ACTIVE = "2" -->
-    [[Autorefresh.Refresh]]: &nbsp;
-    <span id="plugin_autorefresh_left">{INTERVAL}</span> [[AutoRefresh.Min]] &nbsp;
-    {fs:[[AutoRefresh.Deactivate]],,"button"}
-    {fh:"autorefresh_active", 1}
-  <!-- ELSE -->
+  <!-- IF ACTIVE <> "on" -->
     <label for="autorefresh_interval">[[Autorefresh.Refresh]]</label>: &nbsp;
     {ft:"autorefresh_interval",INTERVAL,"small r","size=\"3\""} [[AutoRefresh.Min]] &nbsp;
     {fs:[[AutoRefresh.Activate]],,"button"}
-    {fh:"autorefresh_active", 2}
+    {fh:"autorefresh_active", "on"}
+  <!-- ELSE -->
+    [[Autorefresh.Refresh]]: &nbsp;
+    <span id="plugin_autorefresh_left">{INTERVAL}</span> &nbsp; [[AutoRefresh.Min]] &nbsp;
+    {fs:[[AutoRefresh.Deactivate]],,"button"}
+    {fh:"autorefresh_active", "off"}
   <!-- ENDIF -->
   </form>
 </div>
 
 <script type="text/javascript">
   // <![CDATA[
-  if ("{ACTIVE}" == 2) {
+  if ("{ACTIVE}" == "on") {
     var plugin_autorefresh_start = (new Date).getTime();
-    function plugin_autorefresh_update() {
+    new PeriodicalExecuter( function(pe) {
       // Calc. diff. from load until refresh
       var diff = ((plugin_autorefresh_start - (new Date).getTime())/60000 + {INTERVAL}).toFixed(0);
       if (diff < 0) diff = 0;
@@ -26,15 +26,12 @@
       $("plugin_autorefresh_left").update(diff);
       // Is it time to refresh?
       if (diff == 0) {
+        pe.stop();
         // reload
         location.replace(location.href);
         return;
       }
-      // re-call update...
-      setTimeout("plugin_autorefresh_update();", 60000);
-    }
-    // init update
-    setTimeout("plugin_autorefresh_update();", 60000);
+    }, 60);
   }
   // ]]>
 </script>
--- plugin/autorefresh/plugin.class.php
+++ plugin/autorefresh/plugin.class.php
@@ -13,47 +13,43 @@ Add input to page footer to perform automatic page refresh.
  * @author     Knut Kohl <knutkohl@users.sourceforge.net>
  * @copyright  2009-2011 Knut Kohl
  * @license    GNU General Public License http://www.gnu.org/licenses/gpl.txt
- * @version    1.0.0
  * @version    $Id: v2.4.1-62-gb38404e 2011-01-30 22:35:34 +0100 $
  * @revision   $Rev$
  */
 class esf_Plugin_AutoRefresh extends esf_Plugin {
 
   /**
-   * GET parameter (_PluginAutoRefresh)
-   */
-  const URLPARAM = 'autorefresh_active_set';
-
-  /**
    * @return array Array of events handled by the plugin
    */
   public function handles() {
-    return array('LanguageSet', 'AnalyseRequest', 'OutputStart');
+    return array('LanguageSet', 'AnalysePost', 'OutputStart');
   }
 
   /**
    * Analyse the POST request, extract and remove the parameters
    *
-   * @param &$request array
+   * @param &$post array
    */
-  public function AnalyseRequest( &$request ) {
+  public function AnalysePost( &$post ) {
+    if (!isset($post['autorefresh_active'])) return;
+
     // disable on mobile layouts
     if (Session::get('Mobile') AND !$this->Mobile) return;
-
     if (Registry::get('esf.contentonly')) return;
 
-    if (!isset($request['autorefresh_active'])) return;
+    $active = $post['autorefresh_active'];
+    Session::set('Plugin.Autorefresh.Active', $active);
 
-    Session::set('Plugin.Autorefresh.Active', $request['autorefresh_active']);
-    if ($request['autorefresh_active'] === 2 AND isset($request['autorefresh_interval'])) {
+    if ($active == 'on') {
+      $interval = isset($post['autorefresh_interval'])
+                ? (int) $post['autorefresh_interval']
+                : $this->Interval;
       // store new interval on activation
-      Session::set('Plugin.Autorefresh.Interval', $request['autorefresh_interval']);
+      Session::set('Plugin.Autorefresh.Interval', $interval);
+      TplData::add('HtmlHeader.raw', sprintf('<meta http-equiv="refresh" content="%d">', $interval*60));
+      Messages::Info(Translation::get('AutoRefresh.Enabled', $interval));
     }
-    // redirect to force GET request
-    Core::Redirect(Core::URL(array(
-      'module' => 'auction',
-      'params' => array(self::URLPARAM=>$request['autorefresh_active'])
-    )));
+    Messages::Info(Translation::get('AutoRefresh.Enable_'.$active));
   }
 
   /**
@@ -62,29 +58,16 @@ class esf_Plugin_AutoRefresh extends esf_Plugin {
   public function OutputStart() {
     // disable on mobile layouts
     if (Session::get('Mobile') AND !$this->Mobile) return;
-
     if (Registry::get('esf.contentonly') OR
         Registry::get('esf.module') != 'auction') return;
 
-    $active = (int) Session::get('Plugin.Autorefresh.Active');
-
-    if ($active === 2) {
-      if (!$interval = Session::get('Plugin.Autorefresh.Interval'))
-        $interval = $this->Interval;
-      TplData::add('HtmlHeader.raw', sprintf('<meta http-equiv="refresh" content="%d">', $interval*60));
-      Messages::Info(Translation::get('AutoRefresh.EnableState', $interval));
-    }
-
-    $r = (int) $this->Request(self::URLPARAM);
-    if ($r === 1 OR $r === 2) {
-      Messages::Info(Translation::get('AutoRefresh.Enable_'.$r));
-      $active = $r;
-    }
+    $active = Session::get('Plugin.Autorefresh.Active');
+    $interval = Session::get('Plugin.Autorefresh.Interval');
+    if (!$interval) $interval = $this->Interval;
 
     $data['ACTIVE'] = $active;
-    $interval = Session::get('Plugin.Autorefresh.Interval');
-    $data['INTERVAL']   = $interval ? $interval : $this->Interval;
-    $data['INTERVAL_S'] = $data['INTERVAL']*60;
+    $data['INTERVAL'] = $interval;
+    $data['INTERVAL_S'] = $interval*60;
 
     TplData::add('Footer_After', $this->Render('content', $data));
   }
