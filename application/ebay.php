0cb6907b05efbb2c1c8f6416555538f774c5ea86
Sebastian Muszynski <basti@linkt.de>
2016-05-01 12:50:42 +0200

    Search URL updated. Closes #12.

--- application/ebay.php
+++ application/ebay.php
@@ -25,7 +25,7 @@ Registry::set('ebay.Homepage', 'ebay.'.Registry::get('EbayTLD'));
 // ----------------------------------------------------------------------------
 // Your prefered ebay item page (e.g. of your land), %s for item number
 //
-Registry::set('ebay.ShowUrl', 'http://search.ebay.'.Registry::get('EbayTLD').'/%s');
+Registry::set('ebay.ShowUrl', 'http://www.ebay.'.Registry::get('EbayTLD').'/itm/%s');
 //
 // if you have your own PHProxy running, you can access the auctions via
 // your proxy using this type of URL: (see also docs/TIPS & TRICKS)
