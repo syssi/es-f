37771533ac73effa585f39f354176c0310cf1825
Sebastian Muszynski <basti@linkt.de>
2016-05-01 13:17:46 +0200

    User-Agent updated. https://github.com/syssi/es-f/commit/bf0a7ead061f8d7a6d2dc5ece884a5a1b383d255#commitcomment-17316816

--- application/classes/htmlpage.class.php
+++ application/classes/htmlpage.class.php
@@ -13,7 +13,7 @@ abstract class HTMLpage {
   /**
    *
    */
-  public static $UserAgent = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36';
+  public static $UserAgent = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36';
 
   /**
    *
